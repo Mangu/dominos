06b36412195e1ae8602e09627c7530d6ac4dca2a
Danilo Diaz <ddiaz@live.com>
2025-06-25 09:09:09 -0400

    pwa

--- script.js
+++ script.js
@@ -67,3 +67,12 @@ function updateTimer() {
 setInterval(updateTimer, 1000);
 
 updateDisplay();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+      .catch(err => console.error('Service Worker failed:', err));
+  });
+}
+
